b162b6936d8e4a396f40a050f29a4b82c68159a0
jayashri23 <jsjankar23@gmail.com>
2022-12-06 21:01:43 +0530

    Welcome to Snake and Ladder game

---
+++ src/com/snakeladder/Welcome.java
@@ -0,0 +1,9 @@
+package com.snakeladder;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        System.out.println("Welcome To Snake And Ladder Game");
+        System.out.println("--------------------------------");
+    }
+}
