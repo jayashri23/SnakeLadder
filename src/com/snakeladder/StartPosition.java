36f4ae82076f02772bfcb08d9aad23d6cabeed99
jayashri23 <jsjankar23@gmail.com>
2022-12-06 21:03:38 +0530

    Player at starting Position

---
+++ src/com/snakeladder/StartPosition.java
@@ -0,0 +1,9 @@
+package com.snakeladder;
+
+public class StartPosition {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake Ladder Game");
+        int startPosition = 0;
+        System.out.println("Starting Position =" +startPosition);
+    }
+}
