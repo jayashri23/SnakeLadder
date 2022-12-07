369d88385cf97fcb382a9c215332ff643570617a
jayashri23 <jsjankar23@gmail.com>
2022-12-07 13:53:19 +0530

    the number of

--- src/com/snakeladder/CountDiesPlayerPosition.java
+++ src/com/snakeladder/CountDiesPlayerPosition.java
@@ -2,15 +2,18 @@ package com.snakeladder;
 
 import java.util.Random;
 
-public class ExactWinningPosition {
+public class CountDiesPlayerPosition {
     public static void main(String[] args) {
+
         int start_position = 0;
         int player_position = 0;
         int winning_position = 100;
+        int count =0;
         Random random = new Random();
         System.out.println("Player Starting Position =" + start_position);
         do {
             int dieRoll = random.nextInt(1, 7);
+            count++;
             System.out.println("Rolls Die and get number:" + dieRoll);
 
             //checking for position
@@ -32,7 +35,7 @@ public class ExactWinningPosition {
                 } else {
                     System.out.println("here is snake byte ..& Player moves behind by number of position : " + dieRoll);
                     System.out.println("Player current position : " + player_position);
-               }
+                }
             }
             if (player_position > winning_position){
                 player_position =player_position -dieRoll;
@@ -41,7 +44,9 @@ public class ExactWinningPosition {
                 System.out.println("The number required player : "+(winning_position -player_position));
             }
         }
-        while (player_position !=winning_position && player_position <= winning_position);
-        System.out.println("Player reaches the winning Position");
+        while (player_position !=winning_position );
+        System.out.println("Player reaches the exact winning Position");
+        System.out.println("number of times the dice rolled : "+count);
+        }
     }
-}
+
